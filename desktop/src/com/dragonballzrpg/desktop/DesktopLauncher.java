de062a5ea2d3ac12e7ecb2393e4ae4dfdead0adb
Carl Kavanagh <alterego01010@gmail.com>
2016-08-01 20:35:57 +0100

    Initial project set up

---
+++ desktop/src/com/dragonballzrpg/desktop/DesktopLauncher.java
@@ -0,0 +1,14 @@
+package com.dragonballzrpg.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.dragonballzrpg.DragonballZRPG;
+
+public class DesktopLauncher
+{
+	public static void main(String[] arg)
+	{
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new DragonballZRPG(), config);
+	}
+}
