18598465ce939bcd678414ea354eaeae896eab2b
Carl Kavanagh <alterego01010@gmail.com>
2016-09-20 17:20:32 +0100

    Refactored code

---
+++ core/src/com/dragonballzrpg/states/playerstates/runningstates/RunningSouth.java
@@ -0,0 +1,52 @@
+package com.dragonballzrpg.states.playerstates.runningstates;
+
+import com.badlogic.gdx.Gdx;
+import com.dragonballzrpg.entities.Entity;
+import com.dragonballzrpg.entities.animatedentities.players.Player;
+import com.dragonballzrpg.states.State;
+
+/**
+ * Created by Carl on 24/08/2016.
+ */
+public class RunningSouth extends State
+{
+    @Override
+    public void initialiseTransitions(Player p)
+    {
+
+    }
+
+    @Override
+    public void enter(Entity entity)
+    {
+
+    }
+
+    @Override
+    public void update(Entity entity)
+    {
+        // Down && Left
+        /*if(!((Player)entity).isUpKeyPressed() && ((Player)entity).isDownKeyPressed() &&
+           ((Player)entity).isLeftKeyPressed() && !((Player)entity).isRightKeyPressed())
+        {
+            ((Player)entity).setCurrentAnimation(((Player)entity).getAnimations().get("runningDown"));
+            ((Player)entity).setCurrentState(((Player)entity).getPlayerStates().get("runningSouthWest"));
+        }
+
+        // Down && Right
+        if(!((Player)entity).isUpKeyPressed() && ((Player)entity).isDownKeyPressed() &&
+           !((Player)entity).isLeftKeyPressed() && ((Player)entity).isRightKeyPressed())
+        {
+            ((Player)entity).setCurrentAnimation(((Player)entity).getAnimations().get("runningDown"));
+            ((Player)entity).setCurrentState(((Player)entity).getPlayerStates().get("runningSouthEast"));
+        }
+
+        if(!((Player)entity).isDownKeyPressed())
+        {
+            ((Player)entity).setCurrentAnimation(((Player)entity).getAnimations().get("facingDown"));
+            ((Player)entity).setCurrentState(((Player)entity).getPlayerStates().get("standing"));
+        }*/
+
+        ((Player)entity).position.y -= ((Player)entity).getRunSpeed() * Gdx.graphics.getDeltaTime();
+    }
+}
